6e0a4fa1c7cad03b7059a4dfb716b88b34da5b3b
abdur28 <abdurrahmanidris28@gmail.com>
2024-09-24 09:06:55 +0300

    +

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
             <div className="flex flex-col md:flex-row justify-center p-10 lg:mx-10 gap-5">
                 <div className="md:w-1/3 w-full h-full flex flex-col justify-center gap-2 ">
                     <h1 className="text-2xl font-semibold">TRI FALI SYNDICATE, LTD</h1>
-                    <p className="">This business plan outlines the development of 12 terrace houses on a 3,500-hectare plot in Abuja. With an estimated construction cost of 600 million naira, we aim to deliver a 30% ROI within a year for investors, with minimum investments starting at 80 million naira. The project is scheduled to commence in November 2024 and will cater to upper-middle-class buyers seeking premium residential homes.</p>
+                    <p className="">This business plan outlines the development of 12 terrace houses on a 3,500-hectare plot in Abuja. With an estimated construction cost of 600-800 million naira, we aim to deliver a 15-25% ROI within a year for investors, with minimum investments starting at 40 million naira. The project is scheduled to commence in November 2024 and will cater to upper-middle-class buyers seeking premium residential homes.</p>
                 </div>
                 <div className="md:w-2/3 w-full h-full flex flex-col md:flex-row gap-5 md:gap-0 md:justify-between md:pl-10">
                     <div className="w-full h-full flex flex-col gap-2">
